bbbaf563328b1006897201a27faa0aefcbfdc11f
Ejike Udeze <euworld7@gmail.com>
2020-12-21 14:32:24 -0700

    new commits

--- src/oop/BankAccountApp.java
+++ src/oop/BankAccountApp.java
@@ -6,14 +6,14 @@ public class BankAccountApp {
 		// We want to create a new bank account for RBC clients
 		//instantiate an object
 		BankAccount acc1 = new BankAccount();
-		acc1.accountNumber = "0134568";
-		acc1.setName("Anthony Joshua");
+		acc1.accountNumber = "0134368";
+		acc1.setName("James Joshua");
 		System.out.println(acc1.getName());
 		acc1.balance = 5000;
 		acc1.deposit(4000);
 		acc1.withdraw(2000);
 		System.out.println(acc1.toString());
-		acc1.setSsn("234987045");
+		acc1.setSsn("234987945");
 		System.out.println(acc1.getSsn());
 		acc1.setRate();
 		acc1.newRate();
